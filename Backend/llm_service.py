cb1224db94dbd4e0bb23a0a0dc0f8fb0c54acf40
Utkarsh Agarwal <utkarshagarwal1299@gmail.com>
2026-08-15 16:45:19 +0530

    LLM Model Change
    Model now moved to Env.
    "llama-3.3-70b-versatile" - decommissioned on 16th Aug

--- Backend/llm_service.py
+++ Backend/llm_service.py
@@ -13,7 +13,7 @@ if not GROQ_API_KEY:
     raise ValueError("GROQ_API_KEY is not set in the environment variables.")
 
 client = Groq(api_key=GROQ_API_KEY)
-model = "llama-3.3-70b-versatile"
+model=os.getenv("MODEL")
 
 
 def llm_call(messages):
